57bfe897e8f94c9bee2a9dd2a1e6df5363f94120
Josef Richter <josef.richter@me.com>
2011-12-03 11:05:56 +0100

    updated stats, added chrome frame info

--- main.rb
+++ main.rb
@@ -1,23 +1,33 @@
 require 'rubygems'
 require 'sinatra'
 
+stats = {
+  :worldwide => 10,
+  :africa => 6,
+  :asia => 6,
+  :europe => 10,
+  :northamerica => 13,
+  :australia => 12,
+  :southamerica => 7
+  }
+
 get '/ie-users-info' do
   erb :ie_education_page
 end
 
 get '/' do
-  erb :index
+  erb :index, :locals => stats
 end
 
 get '/join-the-cause' do
 
-  erb :index
+  erb :index, :locals => stats
   
 end
 
 get '/educate-others' do
 
-  erb :index
+  erb :index, :locals => stats
   
 end
 
